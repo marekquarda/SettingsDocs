4901a464baa99222f2626c51e67fd889e1389c46
Marek Xara Quarda <mquarda@centrum.cz>
2025-12-06 21:55:00 +0100

    Create openocd_forStm32_via_orangepi.mk

---
+++ openocd_forStm32_via_orangepi.mk
@@ -0,0 +1,17 @@
+## interface/
+
+```
+adapter driver sysfsgpio
+# Header pin numbers: 16 12 40 38
+# Pins PC4 PD14 PG7 PG6 
+sysfsgpio jtag_nums 68 110 199 198 
+
+# Header pin numbers: 16 12
+sysfsgpio swd_nums 68 110
+
+transport select swd
+
+# Header pin PC07
+sysfsgpio srst_num 71
+reset_config srst_only srst_nogate connect_assert_srst
+```
